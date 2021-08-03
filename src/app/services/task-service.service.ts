bfdaef31e66a14ea0e0f75091a4131d627878156
Toppedyk <Tayloroppedyk@yahoo.com>
2021-08-03 13:09:45 -0600

    saves to db on edit

--- src/app/services/task-service.service.ts
+++ src/app/services/task-service.service.ts
@@ -3,6 +3,13 @@ import { Observable} from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Task } from 'src/app/Task';
 
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  }),
+};
+
 @Injectable({
   providedIn: 'root'
 })
@@ -19,5 +26,10 @@ export class TaskServiceService {
     deleteTask(task: Task): Observable<Task> {
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminder(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions);
   }
 }
